b6aa62ec6b3ac26e69f465948a88931c2ae3896a
Vernon M <vernonmu@gmail.com>
2023-06-25 15:27:41 -0500

    update computer basics endpoint

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.get('/', (req, res) => {
 })
 
 app.get('/computer/basics', (req, res) => {
-    res.json({
-        topic: 'Basics of Computer',
-        content: 'Content about the basics of computers.'
-    })
+    res.send(`
+        <h1>Basics of Computers</h1>
+        <p>Content about the basics of computers or Computer Science 101. Here is a free e-book to get started - <a href="https://cse.unl.edu/~cbourke/ComputerScienceOne.pdf">Computer Science One by Chris Bourke</a></p>
+    `);
 })
+
 
 app.use(function (req, res, next) {
     res.status(404).send(`Sorry, we couldn't find the page: ${req.originalUrl} <br> <button onclick="window.history.back()">Go Back</button>`);
