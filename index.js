ca2466d3f27352997be96611134c5d6bf24d1737
Vernon M <vernonmu@gmail.com>
2023-06-25 14:46:50 -0500

    add index.html

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/computer/basics', (req, res) => {
     })
 })
 
+app.use(function (req, res, next) {
+    res.status(404).send(`Sorry, we couldn't find the page: ${req.originalUrl} <br> <button onclick="window.history.back()">Go Back</button>`);
+});
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} `)
 })
